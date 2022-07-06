0cf9b5f6fb9a67277767a434001a82ecb1c55bfb
Andrew Moore <andrewdmoore84@gmail.com>
2022-07-06 12:51:25 -0400

    Convert shop-all route to use MongoClient rather than Mongoose
    Connecting to MongoDB directly rather than using Mongoose and the Product model allowed for this route to work instead of raising an error related to overwriting the Mongoose model (or it being undefined).
    
    Co-authored-by: Jessica Chang <jesschang77@gmail.com>

---
+++ utils/mongoDb.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb'
+
+let uri = "mongodb+srv://jjam_mongo_pair1:n0A5X0opVo0PJiMq@cluster0.ri64mrj.mongodb.net/?retryWrites=true&w=majority";
+let dbName = "nextJS";
+
+let cachedClient = null
+let cachedDb = null
+
+if (!uri) {
+  throw new Error(
+    'Please define the MONGODB_URI environment variable inside .env.local'
+  )
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGODB_DB environment variable inside .env.local'
+  )
+}
+
+export async function connectToDatabase() {
+  if (cachedClient && cachedDb) {
+    return { client: cachedClient, db: cachedDb }
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  const db = await client.db(dbName)
+
+  cachedClient = client
+  cachedDb = db
+
+  return { client, db }
+}
